e284d50875e464d1a6d89d62c9874ca283de9322
onion213 <kosukekito38@gmail.com>
2021-06-05 22:18:56 +0900

    complete step6

--- steps/step06.py
+++ steps/step06.py
@@ -55,6 +55,17 @@ def f(x):
     C = Square()
     return C(B(A(x)))
 
+A = Square()
+B = Exp()
+C = Square()
+
 x = Variable(np.array(0.5))
-dy = numerical_diff(f, x)
-print(dy)
+a = A(x)
+b = B(a)
+y = C(b)
+
+y.grad = np.array(1.0)
+b.grad = C.backward(y.grad)
+a.grad = B.backward(b.grad)
+x.grad = A.backward(a.grad)
+print(a.grad)
