d78dd23fe8298f1d33101717ea9c8d2d5c8cd50a
onion213 <kosukekito38@gmail.com>
2021-06-06 09:31:12 +0900

    step7 on going. complete 7.1

---
+++ steps/step07.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+
+class Variable:
+    def __init__(self, data):
+        self.data = data
+        self.grad = None
+        self.creator = None
+    def set_creator(self, func):
+        self.creator = func
+
+
+class Function:
+    def __call__(self, input):
+        x = input.data
+        y = self.forward(x)
+        output = Variable(y)
+        output.set_creator(self)
+        self.input = input
+        self.output = output
+        return output
+    
+    def forward(self, x):
+        raise NotImplementedError()
+
+    def backward(self, gy):
+        raise NotImplementedError()
+
+class Square(Function):
+    def forward(self, x):
+        y = x ** 2
+        return y
+    
+    def backward(self, gy):
+        x = self.input.data
+        gx = 2 * x * gy
+        return gx
+
+
+class Exp(Function):
+    def forward(self, x):
+        y = np.exp(x)
+        return y
+
+    def backward(self, gy):
+        x = self.input.data
+        gx = np.exp(x) * gy
+        return gx
