8c61ca6e60bc7e6777b3f2a2ca11b7b8db0813d1
onion <kosukekito38@gmail.com>
2024-08-07 15:01:23 +0900

    feat: Mul and mul for Variable multiplication;

--- dezero/functions/__init__.py
+++ dezero/functions/__init__.py
@@ -1,5 +1,6 @@
 from dezero.functions.add import Add, add
 from dezero.functions.exp import Exp, exp
+from dezero.functions.mul import Mul, mul
 from dezero.functions.square import Square, square
 
-__all__ = ["Exp", "Square", "exp", "square", "Add", "add"]
+__all__ = ["Exp", "Square", "exp", "square", "Add", "add", "Mul", "mul"]
