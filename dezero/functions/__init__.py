5e982ed156c0d1dd9e54c9c06fbad35bf524c701
onion <kosukekito38@gmail.com>
2024-08-08 16:36:08 +0900

    feat: matyas func

--- dezero/functions/__init__.py
+++ dezero/functions/__init__.py
@@ -1,4 +1,6 @@
 from dezero.functions.exp import Exp, exp
+from dezero.functions.matyas import matyas
+from dezero.functions.sphere import sphere
 from dezero.functions.square import Square, square
 
-__all__ = ["Exp", "Square", "exp", "square"]
+__all__ = ["Exp", "Square", "exp", "square", "sphere", "matyas"]
