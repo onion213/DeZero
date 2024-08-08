a89d3a98890883cd691ba7ee2b934b6a984b2398
onion <kosukekito38@gmail.com>
2024-08-09 01:19:15 +0900

    feat: rosenbrock func

--- dezero/functions/__init__.py
+++ dezero/functions/__init__.py
@@ -1,8 +1,21 @@
 from dezero.functions.exp import Exp, exp
 from dezero.functions.goldstein_price import goldstein_price
 from dezero.functions.matyas import matyas
+from dezero.functions.rosenbrock import rosenbrock
 from dezero.functions.sin import Sin, sin, sin_taylor
 from dezero.functions.sphere import sphere
 from dezero.functions.square import Square, square
 
-__all__ = ["Exp", "Square", "exp", "square", "sphere", "matyas", "goldstein_price", "Sin", "sin", "sin_taylor"]
+__all__ = [
+    "Exp",
+    "Square",
+    "exp",
+    "square",
+    "sphere",
+    "matyas",
+    "goldstein_price",
+    "Sin",
+    "sin",
+    "sin_taylor",
+    "rosenbrock",
+]
