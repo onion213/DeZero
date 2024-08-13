f58e2555c7a96ea785f9923243190d7a5ba6f0e8
onion <kosukekito38@gmail.com>
2024-08-13 12:10:19 +0900

    feat: momentum sgd

---
+++ dezero/optimizers/momentum_sgd.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+from dezero.core import Optimizer
+
+
+class MomentumSGD(Optimizer):
+    def __init__(self, lr=0.01, momentum=0.9):
+        super().__init__()
+        self.lr = lr
+        self.momentum = momentum
+        self.vs = {}
+
+    def update_one(self, param):
+        v_key = id(param)
+        if v_key not in self.vs:
+            self.vs[v_key] = np.zeros_like(param.data)
+
+        v = self.vs[v_key]
+        v *= self.momentum
+        v -= self.lr * param.grad.data
+        param.data += v
